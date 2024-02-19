3e164967a960d796d265fc42c4f4ba4f329cfee4
Stepaniuk-Den <stepaniuk.den@gmail.com>
2024-02-19 21:12:28 +0200

    add hero component

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,27 @@
+import "./hero.scss";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="wrapper">
+        <div className="textContainer">
+          <h2>STEPANIUK DENYS</h2>
+          <h1>Web developer</h1>
+          <div className="buttons">
+            <button>See the Latest Works</button>
+            <button>Contact Me</button>
+          </div>
+          <img src="/scroll.png" alt="scroll to bottom" />
+        </div>
+      </div>
+      <div className="slidingTextContainer">
+        Writer Content Creator Influencer
+      </div>
+      <div className="imageContainer">
+        <img src="/hero.png" alt="hero" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
