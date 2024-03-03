31f6b9011b8951f4b759e6ad2618d4ef70886e5a
Stepaniuk-Den <stepaniuk.den@gmail.com>
2024-03-03 11:39:05 +0200

    update navbar link

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,9 @@
 import "./navbar.scss";
 import { motion } from "framer-motion";
 import Sidebar from "../sidebar/Sidebar";
+import Telegram from "../../assets/icons/telegram.svg";
+import Linkedin from "../../assets/icons/linkedin.svg";
+import Github from "../../assets/icons/github copy.svg";
 
 const Navbar = () => {
   return (
@@ -15,17 +18,26 @@ const Navbar = () => {
           Stepaniuk Den
         </motion.span>
         <div className="social">
-          <a href="#">
-            <img src="/facebook.png" alt="facebook" />
+          <a
+            href="https://t.me/StepaniukDenys"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={Telegram} alt="telegram" />
           </a>
-          <a href="#">
-            <img src="/instagram.png" alt="instagram" />
+          <a
+            href="https://www.linkedin.com/in/denys-stepaniuk/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={Linkedin} alt="linkedin" />
           </a>
-          <a href="#">
-            <img src="/youtube.png" alt="youtube" />
-          </a>
-          <a href="#">
-            <img src="/dribbble.png" alt="dribbble" />
+          <a
+            href="https://github.com/Stepaniuk-Den"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={Github} alt="github" />
           </a>
         </div>
       </div>
